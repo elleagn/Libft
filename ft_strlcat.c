62bf165feb86676cbdbea8b24befecd7d8f60a75
Gaelle Ozon <gozon@bess-f2r2s10.clusters.42paris.fr>
2024-05-20 16:05:31 +0200

    Add ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,28 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/20 10:18:51 by gozon             #+#    #+#             */
-/*   Updated: 2024/05/20 16:03:09 by gozon            ###   ########.fr       */
+/*   Created: 2024/05/20 15:55:45 by gozon             #+#    #+#             */
+/*   Updated: 2024/05/20 16:01:19 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memset(void *s, int c, size_t n)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
-	unsigned char *p;
 
-	p = s;
 	i = 0;
-	while (i < n)
+	while (s[i])
 	{
-		p[i] = (unsigned char) c;
 		i++;
 	}
-	return (s);
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t siz)
+{
+	size_t	dest_length;
+	size_t	src_length;
+	size_t	i;
+
+	dest_length = ft_strlen(dest);
+	src_length = ft_strlen(src);
+	if (dest_length >= size)
+		return (size + src_length);
+	i = 0;
+	while (i < size - dest_length - 1 && src[i])
+	{
+		dest[dest_length + i] = src[i];
+		i++;
+	}
+	dest[dest_length + i] = '\0';
+	return (dest_length + src_length);
 }

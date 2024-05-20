38b6e1573cc963e2bdbe3361222a2fdd8e0451fc
Gaelle Ozon <gozon@bess-f2r2s10.clusters.42paris.fr>
2024-05-20 17:12:39 +0200

    Add ft_strchr and ft_strrchr

---
+++ ft_strchr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 16:33:45 by gozon             #+#    #+#             */
+/*   Updated: 2024/05/20 16:55:07 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] == c)
+		{
+			return ((char *) &s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}

6fd24e3ff62ff925beb68d811fd9cf166dff1089
Gaelle Ozon <gozon@bess-f2r2s10.clusters.42paris.fr>
2024-05-27 11:17:05 +0200

    Added ft_lstdelone

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 10:42:55 by gozon             #+#    #+#             */
-/*   Updated: 2024/05/27 10:59:06 by gozon            ###   ########.fr       */
+/*   Created: 2024/05/27 10:52:53 by gozon             #+#    #+#             */
+/*   Updated: 2024/05/27 11:05:27 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	del(lst->content);
-	free(lst);
+	t_list	*next;
+
+	while ((*lst)->next)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		lst = next;
+	}
+	ft_lstdelone(*lst, del);
+	(*lst) = NULL;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 11:39:10 by gozon             #+#    #+#             */
-/*   Updated: 2024/05/27 10:41:23 by gozon            ###   ########.fr       */
+/*   Updated: 2024/05/27 11:16:23 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,5 +72,8 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
